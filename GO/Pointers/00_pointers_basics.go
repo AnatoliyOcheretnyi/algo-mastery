1d40c412ce0b34c79be02863eb920b2fe99cbf39
AnatoliyOcheretnyi <svito014@gmail.com>
2026-01-14 22:51:56 +0200

    added new tasks related to pointers

--- GO/Pointers/00_pointers_basics.go
+++ GO/Pointers/00_pointers_basics.go
@@ -12,6 +12,9 @@ Tasks:
 4) incrementPtr(p *int): add 1 to the value via pointer.
 5) updateName(u *User, name string): update struct field via pointer.
 6) setFirst(nums []int, v int): set first element (note: slices are references).
+7) setNilToZero(p **int): if *p is nil, allocate and set to 0.
+8) appendViaPointer(nums *[]int, v int): append value to slice via pointer.
+9) resetUser(u *User): set Name="" and Age=0 via pointer.
 */
 
 type User struct {
@@ -43,6 +46,18 @@ func setFirst(nums []int, v int) {
 	// TODO: implement
 }
 
+func setNilToZero(p **int) {
+	// TODO: implement
+}
+
+func appendViaPointer(nums *[]int, v int) {
+	// TODO: implement
+}
+
+func resetUser(u *User) {
+	// TODO: implement
+}
+
 func main() {
 	// Task 1/2
 	x := 5
@@ -69,4 +84,19 @@ func main() {
 	nums := []int{9, 8, 7}
 	setFirst(nums, 1)
 	fmt.Println("Task 6:", nums, "expected", []int{1, 8, 7})
+
+	// Task 7
+	var p *int
+	setNilToZero(&p)
+	fmt.Println("Task 7:", *p, "expected", 0)
+
+	// Task 8
+	list := []int{1, 2}
+	appendViaPointer(&list, 3)
+	fmt.Println("Task 8:", list, "expected", []int{1, 2, 3})
+
+	// Task 9
+	u2 := User{Name: "Bob", Age: 40}
+	resetUser(&u2)
+	fmt.Println("Task 9:", u2, "expected", "{ 0}")
 }
